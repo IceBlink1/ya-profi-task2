652a71393900a8809ab93e49f6af4d798ec114c3
Alexander Lyutikov <lyutiko.alex@gmail.com>
2021-03-14 12:29:22 +0300

    Fixed api endpoints

--- notes/urls.py
+++ notes/urls.py
@@ -3,6 +3,6 @@ from django.conf.urls import url
 from notes import views
 
 urlpatterns = [
-    url(r'^api/notes$', views.notes),
-    url(r'^api/notes/(?P<pk>[0-9]+)$', views.note_detail),
+    url(r'^notes$', views.notes),
+    url(r'^notes/(?P<pk>[0-9]+)$', views.note_detail),
 ]
